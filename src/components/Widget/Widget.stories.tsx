dca1c29a77047faa03877f500f488c22c3018546
Dmytro <dmitry.tkk@gmail.com>
2023-04-10 16:47:21 +0100

    Add mainnet endpoint env

--- src/components/Widget/Widget.stories.tsx
+++ src/components/Widget/Widget.stories.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Widget } from "./Widget";
 
 const METAPORT_CONFIG = require('../../configs/metaportConfigMainnet.json');
+METAPORT_CONFIG.mainnetEndpoint = process.env.STORYBOOK_MAINNET_ENDPOINT;
 
 
 export default {
