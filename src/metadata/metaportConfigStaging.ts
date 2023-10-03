c8177b039ac0a825d477c40c388e153e4e48007d
Dmytro <dmitry.tkk@gmail.com>
2023-10-03 13:14:01 +0100

    Use legacy config

--- src/metadata/metaportConfigStaging.ts
+++ src/metadata/metaportConfigStaging.ts
@@ -1,17 +1,14 @@
 import * as interfaces from '../core/interfaces'
 
 export const METAPORT_CONFIG: interfaces.MetaportConfig = {
-  skaleNetwork: 'staging',
+  skaleNetwork: 'legacy',
   openOnLoad: true,
   openButton: true,
   debug: false,
   chains: [
     'mainnet',
-    'staging-legal-crazy-castor', // Europa
-    'staging-utter-unripe-menkar', // Calypso
-    'staging-faint-slimy-achird', // Nebula
-    'staging-perfect-parallel-gacrux', // Test Chain 1
-    'staging-severe-violet-wezen' // Test Chain 2
+    'skale-innocent-nasty', // europa
+    'international-villainous-zaurak' // calypso
   ],
   tokens: {
     eth: {
@@ -26,47 +23,6 @@ export const METAPORT_CONFIG: interfaces.MetaportConfig = {
       decimals: '6',
       symbol: 'USDC',
       name: 'USD Coin'
-    },
-    usdt: {
-      decimals: '6',
-      symbol: 'USDT',
-      name: 'Tether USD'
-    },
-    wbtc: {
-      decimals: '18',
-      symbol: 'WBTC',
-      name: 'WBTC'
-    },
-    _SPACE_1: {
-      name: 'SKALE Space',
-      symbol: 'SPACE',
-      iconUrl:
-        'https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Rocket/3D/rocket_3d.png'
-    },
-    _SKALIENS_1: {
-      name: 'SKALIENS Collection',
-      symbol: 'SKALIENS',
-      iconUrl:
-        'https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Alien/3D/alien_3d.png'
-    },
-    ruby: {
-      name: 'Ruby Token',
-      iconUrl: 'https://ruby.exchange/images/tokens/ruby-square.png',
-      symbol: 'RUBY'
-    },
-    dai: {
-      name: 'DAI Stablecoin',
-      symbol: 'DAI'
-    },
-    usdp: {
-      name: 'Pax Dollar',
-      symbol: 'USDP',
-      iconUrl: 'https://ruby.exchange/images/tokens/usdp-square.png'
-    },
-    hmt: {
-      name: 'Human Token',
-      symbol: 'HMT',
-      iconUrl: 'https://s2.coinmarketcap.com/static/img/coins/64x64/10347.png'
     }
   },
   connections: {
@@ -74,152 +30,78 @@ export const METAPORT_CONFIG: interfaces.MetaportConfig = {
       eth: {
         eth: {
           chains: {
-            'staging-legal-crazy-castor': {},
-            'staging-utter-unripe-menkar': {
-              hub: 'staging-legal-crazy-castor'
+            'skale-innocent-nasty': {},
+            'international-villainous-zaurak': {
+              hub: 'skale-innocent-nasty'
             }
           }
         }
       },
       erc20: {
         skl: {
-          address: '0x493D4442013717189C9963a2e275Ad33bfAFcE11',
+          address: '0x17A7Cf31a11554e75246973663262dA56F84F89b',
           chains: {
-            'staging-legal-crazy-castor': {},
-            'staging-utter-unripe-menkar': {
-              hub: 'staging-legal-crazy-castor'
-            },
-            'staging-faint-slimy-achird': {
-              hub: 'staging-legal-crazy-castor'
+            'skale-innocent-nasty': {},
+            'international-villainous-zaurak': {
+              hub: 'skale-innocent-nasty'
             }
           }
         },
-        ruby: {
-          address: '0xd66641E25E9D36A995682572eaD74E24C11Bb422',
-          chains: {
-            'staging-legal-crazy-castor': {}
-          }
-        },
-        dai: {
-          address: '0x83B38f79cFFB47CF74f7eC8a5F8D7DD69349fBf7',
-          chains: {
-            'staging-legal-crazy-castor': {}
-          }
-        },
-        usdp: {
-          address: '0x66259E472f8d09083ecB51D42F9F872A61001426',
-          chains: {
-            'staging-legal-crazy-castor': {}
-          }
-        },
-        usdt: {
-          address: '0xD1E44e3afd6d3F155e7704c67705E3bAC2e491b6',
-          chains: {
-            'staging-legal-crazy-castor': {}
-          }
-        },
-        usdc: {
-          address: '0x85dedAA65D33210E15911Da5E9dc29F5C93a50A9',
-          chains: {
-            'staging-legal-crazy-castor': {},
-            'staging-utter-unripe-menkar': {
-              hub: 'staging-legal-crazy-castor'
-            }
-          }
-        },
-        wbtc: {
-          address: '0xd80BC0126A38c9F7b915e1B2B9f78280639cadb3',
-          chains: {
-            'staging-legal-crazy-castor': {}
-          }
-        },
-        hmt: {
-          address: '0x4058d058ff62ED347dB8a69c43Ae9C67268B50b0',
-          chains: {}
-        }
-      },
-      erc721meta: {
-        _SPACE_1: {
-          address: '0x1b7729d7E1025A031aF9D6E68598b57f4C2adfF6',
-          chains: {}
-        }
-      },
-      erc1155: {
-        _SKALIENS_1: {
-          address: '0x6cb73D413970ae9379560aA45c769b417Fbf33D6',
-          chains: {}
-        }
+        // usdc: {
+        //   address: '0x85dedAA65D33210E15911Da5E9dc29F5C93a50A9',
+        //   chains: {
+        //     'skale-innocent-nasty': {},
+        //     'international-villainous-zaurak': {
+        //       hub: 'skale-innocent-nasty'
+        //     }
+        //   }
+        // }
       }
     },
-    'staging-utter-unripe-menkar': {
+    'international-villainous-zaurak': {
       // Calypso connections
       eth: {
         eth: {
-          address: '0xECabAE592Eb56D96115FcF4c7F772ADB7BF573d0',
+          address: '0x9C0e8bC2B2D403299214c80081F93fAB5e10b593',
           chains: {
-            'staging-legal-crazy-castor': {
+            'skale-innocent-nasty': {
               clone: true
             },
             mainnet: {
               clone: true,
-              hub: 'staging-legal-crazy-castor'
+              hub: 'skale-innocent-nasty'
             }
           }
         }
       },
       erc20: {
         skl: {
-          address: '0x7E1B8750C21AebC3bb2a0bDf40be104C609a9852',
+          address: '0xFbbDF9aC97093b1E88aB79F7D0c296d9cc5eD0d0',
           chains: {
-            'staging-legal-crazy-castor': {
-              clone: true
-            },
-            'staging-faint-slimy-achird': {
-              hub: 'staging-legal-crazy-castor',
+            'skale-innocent-nasty': {
               clone: true
             },
             mainnet: {
-              hub: 'staging-legal-crazy-castor',
+              hub: 'skale-innocent-nasty',
               clone: true
             }
           }
         },
-        usdc: {
-          address: '0x49c37d0Bb6238933eEe2157e9Df417fd62723fF6',
-          chains: {
-            'staging-legal-crazy-castor': {
-              clone: true
-            },
-            mainnet: {
-              hub: 'staging-legal-crazy-castor',
-              clone: true
-            }
-          }
-        }
+        // usdc: {
+        //   address: '0x49c37d0Bb6238933eEe2157e9Df417fd62723fF6',
+        //   chains: {
+        //     'skale-innocent-nasty': {
+        //       clone: true
+        //     },
+        //     mainnet: {
+        //       hub: 'skale-innocent-nasty',
+        //       clone: true
+        //     }
+        //   }
+        // }
       }
     },
-    'staging-faint-slimy-achird': {
-      // Nebula connections
-      erc20: {
-        skl: {
-          address: '0x7F73B66d4e6e67bCdeaF277b9962addcDabBFC4d',
-          chains: {
-            'staging-legal-crazy-castor': {
-              clone: true
-            },
-            mainnet: {
-              hub: 'staging-legal-crazy-castor',
-              clone: true
-            },
-            'staging-utter-unripe-menkar': {
-              hub: 'staging-legal-crazy-castor',
-              clone: true
-            }
-          }
-        }
-      }
-    },
-    'staging-legal-crazy-castor': {
+    'skale-innocent-nasty': {
       // Europa connections
       eth: {
         eth: {
@@ -228,87 +110,36 @@ export const METAPORT_CONFIG: interfaces.MetaportConfig = {
             mainnet: {
               clone: true
             },
-            'staging-utter-unripe-menkar': {
-              wrapper: '0xa270484784f043e159f74C03B691F80B6F6e3c24'
+            'international-villainous-zaurak': {
+              wrapper: '0x321e1aa81B4c6CC3B8EFe3D9c0AD67E6eC949c2c'
             }
           }
         }
       },
       erc20: {
         skl: {
-          address: '0xbA1E9BA7CDd4815Da6a51586bE56e8643d1bEAb6',
+          address: '0xa101902B3119f4830292bb79ebAB56967229207B',
           chains: {
             mainnet: {
               clone: true
             },
-            'staging-utter-unripe-menkar': {
-              wrapper: '0x6a679eF80aF3fE01A646F858Ca1e26D58b5430B6'
-            },
-            'staging-faint-slimy-achird': {
-              wrapper: '0x6a679eF80aF3fE01A646F858Ca1e26D58b5430B6'
+            'international-villainous-zaurak': {
+              wrapper: '0x51A1eD016633Afb00C25Eb404745C61D8c16BBd4'
             }
           }
         },
-        ruby: {
-          address: '0xf06De9214B1Db39fFE9db2AebFA74E52f1e46e39',
-          chains: {
-            mainnet: {
-              clone: true
-            }
-          }
-        },
-        dai: {
-          address: '0x3595E2f313780cb2f23e197B8e297066fd410d30',
-          chains: {
-            mainnet: {
-              clone: true
-            }
-          }
-        },
-        usdp: {
-          address: '0xe0E2cb3A5d6f94a5bc2D00FAa3e64460A9D241E1',
-          chains: {
-            mainnet: {
-              clone: true
-            }
-          }
-        },
-        usdt: {
-          address: '0xa388F9783d8E5B0502548061c3b06bf4300Fc0E1',
-          chains: {
-            mainnet: {
-              clone: true
-            }
-          }
-        },
-        usdc: {
-          address: '0x5d42495D417fcd9ECf42F3EA8a55FcEf44eD9B33',
-          chains: {
-            mainnet: {
-              clone: true
-            },
-            'staging-utter-unripe-menkar': {
-              wrapper: '0x4f250cCE5b8B39caA96D1144b9A32E1c6a9f97b0'
-            }
-          }
-        },
-        wbtc: {
-          address: '0xf5E880E1066DDc90471B9BAE6f183D5344fd289F',
-          chains: {
-            mainnet: {
-              clone: true
-            }
-          }
-        }
+        // usdc: {
+        //   address: '0x5d42495D417fcd9ECf42F3EA8a55FcEf44eD9B33',
+        //   chains: {
+        //     mainnet: {
+        //       clone: true
+        //     },
+        //     'international-villainous-zaurak': {
+        //       wrapper: '0x4f250cCE5b8B39caA96D1144b9A32E1c6a9f97b0'
+        //     }
+        //   }
+        // }
       }
-    },
-    'staging-severe-violet-wezen': {
-      erc20: {}
-    },
-    'staging-perfect-parallel-gacrux': {
-      erc20: {},
-      erc721: {},
-      erc1155: {}
     }
   },
   theme: {
