a4c29d31a4e3369d97bddf22528f863029304fe3
Dmytro <dmitry.tkk@gmail.com>
2023-10-20 19:27:54 +0100

    Improve error handling

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,3 +1,8 @@
+import { useState } from 'react'
+
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
 import Button from '@mui/material/Button'
 
 import { cls, cmn, styles } from '../core/css'
@@ -8,15 +13,32 @@ import LinkOffRoundedIcon from '@mui/icons-material/LinkOffRounded'
 import PublicOffRoundedIcon from '@mui/icons-material/PublicOffRounded'
 import SentimentDissatisfiedRoundedIcon from '@mui/icons-material/SentimentDissatisfiedRounded'
 import ErrorRoundedIcon from '@mui/icons-material/ErrorRounded'
+import HourglassTopRoundedIcon from '@mui/icons-material/HourglassTopRounded';
+import CrisisAlertRoundedIcon from '@mui/icons-material/CrisisAlertRounded';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import TextSnippetRoundedIcon from '@mui/icons-material/TextSnippetRounded';
+import HourglassBottomRoundedIcon from '@mui/icons-material/HourglassBottomRounded';
+import AvTimerRoundedIcon from '@mui/icons-material/AvTimerRounded';
+
+import { DEFAULT_ERROR_MSG } from '../core/constants'
 
 const ERROR_ICONS = {
   'link-off': <LinkOffRoundedIcon />,
   'public-off': <PublicOffRoundedIcon />,
   sentiment: <SentimentDissatisfiedRoundedIcon />,
-  error: <ErrorRoundedIcon />
+  warning: <CrisisAlertRoundedIcon color='warning' />,
+  error: <ErrorRoundedIcon />,
+  time: <HourglassTopRoundedIcon />
 }
 
 export default function Error(props: { errorMessage: ErrorMessage }) {
+
+  const [expanded, setExpanded] = useState<string | false>(false)
+
+  const handleChange = (panel: string) => (_: React.SyntheticEvent, isExpanded: boolean) => {
+    setExpanded(isExpanded ? panel : false)
+  }
+
   if (!props.errorMessage) return
   return (
     <div>
@@ -24,23 +46,64 @@ export default function Error(props: { errorMessage: ErrorMessage }) {
         {ERROR_ICONS[props.errorMessage.icon]}
       </div>
       <p
-        style={{ wordBreak: 'break-all' }}
+        style={{ wordBreak: 'break-word' }}
         className={cls(cmn.p1, cmn.p, cmn.p600, cmn.pPrim, cmn.flexg, cmn.pCent, cmn.mtop10)}
       >
-        Oops! Something went wrong.
+        {props.errorMessage.headline ?? DEFAULT_ERROR_MSG}
       </p>
-      <p className={cls(cmn.p4, cmn.p, cmn.p600, cmn.pSec, cmn.flexg, cmn.pCent, cmn.mbott10)}>
+      <p className={cls(cmn.p4, cmn.p, cmn.p500, cmn.pSec, cmn.flexg, cmn.pCent, cmn.mbott10)}>
         Logs are available in your browser's developer console
       </p>
-      <div className={cmn.flex}>
-        <p
-          style={{ wordBreak: 'break-all' }}
-          className={cls(cmn.p3, cmn.p, cmn.pPrim, cmn.flexg, cmn.pCent, cmn.mtop10, cmn.mbott20)}
-        >
-          {props.errorMessage.text}
+
+      <div className={cls(cmn.flex, cmn.fullWidth, cmn.flexcv, cmn.mtop20, cmn.mbott10, cmn.mleft10)}>
+        <div className={cls(cmn.flex, cmn.flexc, cmn.mri10)}>
+          <AvTimerRoundedIcon color="primary" />
+        </div>
+        <p className={cls(cmn.p, cmn.p3, cmn.p600, cmn.pPrim, cmn.mri10)}>
+          When transferring from SKALE to Ethereum Mainnet, there are frequency limitations.
+        </p>
+      </div>
+      <div className={cls(cmn.flex, cmn.fullWidth, cmn.flexcv, cmn.mtop20, cmn.mbott10, cmn.mleft10)}>
+        <div className={cls(cmn.flex, cmn.flexc, cmn.mri10)}>
+          <HourglassBottomRoundedIcon color="primary" />
+        </div>
+        <p className={cls(cmn.p, cmn.p3, cmn.p600, cmn.pPrim, cmn.mri10)}>
+          Sometimes transfers may take more time than expected.
         </p>
       </div>
 
+      <Accordion
+        expanded={expanded === 'panel1'}
+        onChange={handleChange('panel1')}
+      >
+        <AccordionSummary
+          className={cls(styles.accordionSummarySm, styles.accordionSm)}
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <div className={cls(cmn.flex, cmn.fullWidth, cmn.flexcv)}>
+            <div className={cls(cmn.flex, cmn.flexc, cmn.mri10)}>
+              <TextSnippetRoundedIcon color="warning" />
+            </div>
+            <p className={cls(cmn.p, cmn.p3, cmn.p600, cmn.cap, cmn.pPrim, cmn.mri10)}>
+              {expanded === 'panel1' ? 'Hide' : 'Show'} error details
+            </p>
+          </div>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div className={cmn.mbott20}>
+            <code
+              style={{ wordBreak: 'break-all' }}
+              className={cls(cmn.p4, cmn.p, cmn.pPrim, cmn.flexg,
+                cmn.pCent, cmn.mleft10, cmn.mri10, cmn.mbott20)}
+            >
+              {props.errorMessage.text}
+            </code>
+          </div>
+
+        </AccordionDetails>
+      </Accordion>
       {props.errorMessage.fallback ? (
         <Button
           variant="contained"
