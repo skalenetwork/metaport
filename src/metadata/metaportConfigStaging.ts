32dd3cf08e2a8f7c6536ae7ff7c9e0b1503fcfc3
Dmytro <dmitry.tkk@gmail.com>
2023-10-23 19:00:41 +0100

    Fix wrapped tokens lookup

--- src/metadata/metaportConfigStaging.ts
+++ src/metadata/metaportConfigStaging.ts
@@ -6,8 +6,8 @@ export const METAPORT_CONFIG: interfaces.MetaportConfig = {
   openButton: true,
   debug: true,
   chains: [
-    //'mainnet',
     'rural-colossal-cebalrai',
+    'mainnet',
     'skale-innocent-nasty', // europa
     'international-villainous-zaurak', // calypso
     'big-majestic-oval-SKALE' // QA chain
@@ -28,6 +28,7 @@ export const METAPORT_CONFIG: interfaces.MetaportConfig = {
     }
   },
   connections: {
+    'rural-colossal-cebalrai': {},
     mainnet: {
       eth: {
         eth: {
